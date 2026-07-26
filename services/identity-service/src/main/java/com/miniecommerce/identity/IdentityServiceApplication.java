f4892b3cfe946e47dedc7e0c70a6463e861efe7f
duocnguyen058 <dcnguyn058@gmail.com>
2026-07-26 15:03:16 +0700

    identity-service: thêm @EnableConfigurationProperties cho JwtConfig

--- services/identity-service/src/main/java/com/miniecommerce/identity/IdentityServiceApplication.java
+++ services/identity-service/src/main/java/com/miniecommerce/identity/IdentityServiceApplication.java
@@ -2,8 +2,12 @@ package com.miniecommerce.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.miniecommerce.identity.config.JwtConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtConfig.class)
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
