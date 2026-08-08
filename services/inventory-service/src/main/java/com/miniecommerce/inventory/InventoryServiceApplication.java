fb864d3ddba176e3742b7efe3cc4a412d5899329
duocnguyen058 <dcnguyn058@gmail.com>
2026-08-08 22:08:47 +0700

    Cấu hình inventory-service: pom, class khởi động, application và Dockerfile

--- services/inventory-service/src/main/java/com/miniecommerce/inventory/InventoryServiceApplication.java
+++ services/inventory-service/src/main/java/com/miniecommerce/inventory/InventoryServiceApplication.java
@@ -2,8 +2,15 @@ package com.miniecommerce.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+import com.miniecommerce.inventory.shared.config.JwtConfig;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+@EnableConfigurationProperties(JwtConfig.class)
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
