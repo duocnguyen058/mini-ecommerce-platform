6d388995ed9e67fd898a23c742137bfc8507ff07
duocnguyen058 <dcnguyn058@gmail.com>
2026-08-08 22:09:05 +0700

    Cấu hình order-service: pom, class khởi động, application và Dockerfile

--- services/order-service/src/main/java/com/miniecommerce/order/OrderServiceApplication.java
+++ services/order-service/src/main/java/com/miniecommerce/order/OrderServiceApplication.java
@@ -2,8 +2,18 @@ package com.miniecommerce.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.miniecommerce.order.shared.config.JwtConfig;
+import com.miniecommerce.order.shared.config.OrderProperties;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+@EnableConfigurationProperties({ JwtConfig.class, OrderProperties.class })
+@EnableScheduling
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
