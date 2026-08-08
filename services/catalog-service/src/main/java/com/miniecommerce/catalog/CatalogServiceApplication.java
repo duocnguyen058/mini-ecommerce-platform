72c2924c6ecf9442088db65f329412031dce16a1
duocnguyen058 <dcnguyn058@gmail.com>
2026-08-08 22:08:04 +0700

    Cập nhật cấu hình catalog-service: pom, class khởi động, application và thêm Dockerfile

--- services/catalog-service/src/main/java/com/miniecommerce/catalog/CatalogServiceApplication.java
+++ services/catalog-service/src/main/java/com/miniecommerce/catalog/CatalogServiceApplication.java
@@ -2,11 +2,13 @@ package com.miniecommerce.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+@ConfigurationPropertiesScan("com.miniecommerce.catalog.config")
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
