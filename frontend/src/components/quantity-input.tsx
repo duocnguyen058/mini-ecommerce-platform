ef57e283bc856256782c4e5e76d14488e564a452
duocnguyen058 <dcnguyn058@gmail.com>
2026-08-21 11:43:05 +0700

    style(components): cải tiến giao diện QuantityInput với nút tăng giảm số lượng

--- frontend/src/components/quantity-input.tsx
+++ frontend/src/components/quantity-input.tsx
@@ -28,7 +28,7 @@ export function QuantityInput({
   const [inputValue, setInputValue] = useState<string>(String(value));
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
+     
     setInputValue(String(value));
   }, [value]);
 
