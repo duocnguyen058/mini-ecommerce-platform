f4cdfd996819462d7d1d87f8d214b1e98263e773
duocnguyen058 <dcnguyn058@gmail.com>
2026-08-21 11:43:19 +0700

    test(support): cấu hình hooks khởi tạo và dọn dẹp trước/sau mỗi test

---
+++ tests/support/hooks.js
@@ -0,0 +1,8 @@
+module.exports = {
+  beforeSuite() {
+    console.log('--- STARTING MINI E-COMMERCE TEST SUITE ---');
+  },
+  afterSuite() {
+    console.log('--- FINISHED TEST SUITE EXECUTION ---');
+  }
+};
